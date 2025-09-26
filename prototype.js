79c68b560f1440a261556fa07cd9c5fbc6ddb372
Tarik Bufardii <bufardiitarik@gmail.com>
2025-09-26 11:53:27 +0100

    correction

--- prototype.js
+++ prototype.js
@@ -1,24 +1,23 @@
 //prototype
 
-function readthis(infos) {
-  let choosingcode = prompt("Which one do you want to count?");
-  choosingcode = choosingcode.toUpperCase();
-
-  let count = 0;
-
-  for (let i = 0; i < infos.length; i++) {
-    if (infos[i] === choosingcode) {
-      count++;
-    }
-  }
-
-  console.log(`The code "${choosingcode}" appears ${count} times.`);
-}
-
 let codes = ["C1", "C2", "C1", "C3", "C2", "C1"];
 
+let choosingcode = prompt("Which code do you want to count? (e.g., C1)");//what gonna shows on window
 
-readthis(codes);
+choosingcode = choosingcode.toUpperCase();// for min and maj letters
+
+let count = 0;
+
+for (let i = 0; i < codes.length; i++) {
+  if (codes[i] === choosingcode) {
+    count++;
+  }
+}
+
+
+console.log(`The code "${choosingcode}" appears ${count} times.`);
+
+
 
 
 
